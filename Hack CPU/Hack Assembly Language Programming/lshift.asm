49c029c44fa7abb97f9d16667566dec8a7a6ac8c
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2023-11-05 16:40:43 -0500

    Fixed lshift.asm

--- Hack CPU/Hack Assembly Language Programming/lshift.asm
+++ Hack CPU/Hack Assembly Language Programming/lshift.asm
@@ -1,37 +1,42 @@
 @0
 D = A // D <- 0
-@18 // c
+@18 // c, stores result
 M = D // c <- 0
-@17
-D = M // for inner loop
-@19 
-M = D // for outer loop
-@20
-M = D // to keep lshift value
+@2
+D=A
+@20 // holds onto 2
+M = D
+D = M
 (OUTERLOOP)
-    @19
+    @19 // number of times left shifting
     D = M
     @ENDOUTER
     D;JLE
+    @18
+    M = 0
     (INNERLOOP)
-        @17 // b
+        @20 // b
         D = M; // D <- b
         @ENDINNER
         D; JLE // if B <= 0, goto END
         @16 // a
         D = M // D <- a
         @18 // c
-        M = M+D // c <- c + a
-        @17 // b
-        M = M-1 // b <- b-1
+        M = M + D // c <- c + a
+        @20 // b
+        M = M - 1  // b <- b-1
         @INNERLOOP
         0;JMP // loop again, goto LOOP
     (ENDINNER)
-    @19
-    M=M-1
-    @20
+    @18
     D = M
-    @17
+    @16
+    M = D
+    @19
+    M = M - 1
+    @2
+    D = A
+    @20
     M = D
     @OUTERLOOP
     0;JMP
